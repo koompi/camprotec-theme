24aaae52a0a6738716ace4b7fbe29efbaba06017
vannsoklay <soklayvann@gmail.com>
2024-03-28 14:30:18 +0700

    update

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -337,6 +337,8 @@ export default function ProductsPage({
     },
   })
 
+  // 
+
   return (
     <section className="container mx-auto px-3 sm:px-3 lg:px-6 py-3 sm:py-3 lg:py-9">
       <div className="flex gap-x-6">
