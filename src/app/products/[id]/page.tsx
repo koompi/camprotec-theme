f34f03ef8b637bac051d21b8b59773ce8ec69384
Thith-thith <thiththin762@gmail.com>
2024-04-20 00:23:04 +0700

    update single page product

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,25 +1,75 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import ProductViewInfo from "./components/ProductViewItem";
-import { useQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import { GET_PRODUCT } from "@/graphql/product";
+import { Metadata, ResolvingMetadata } from "next";
+import { getClient } from "@/libs/client";
 
-export default function ProductPage() {
-  const param = useParams();
-  
-  const { data, loading } = useQuery(GET_PRODUCT, {
-    variables: {
-      slug: param.id
-    }
-  })
-  if(loading){
-    return <div>loading...</div>
-  }
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+
+  const client = getClient();
+  const { data } = await client.query({
+    query: GET_PRODUCT,
+    variables: { slug: id },
+  });
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: data?.storeProduct?.title,
+    description: data?.storeProduct?.desc,
+
+    openGraph: {
+      title: {
+        default: data?.storeProduct?.title,
+        template: `%s - ${data?.storeProduct?.title}`,
+      },
+      description: data?.storeProduct?.desc,
+      images: [
+        {
+          url:
+            process.env.NEXT_PUBLIC_DRIVE ??
+            `https://drive.backnd.riverbase.org/api/drive?hash=${data?.storeProduct?.previews[0]}`,
+          width: 800,
+          height: 600,
+        },
+        {
+          url:
+            process.env.NEXT_PUBLIC_DRIVE ??
+            `https://drive.backnd.riverbase.org/api/drive?hash=${data?.storeProduct?.previews[0]}`,
+          width: 1800,
+          height: 1600,
+        },
+        ...previousImages,
+      ],
+      locale: "en-US",
+      type: "website",
+    },
+  };
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const client = getClient();
+  const { data } = await client.query({
+    query: GET_PRODUCT,
+    variables: { slug: params.id },
+  });
+
   return (
     <section className="container mx-auto px-6 py-3 sm:py-3 lg:py-16">
       <ProductViewInfo {...data?.storeProduct} />
     </section>
   );
 }
-
